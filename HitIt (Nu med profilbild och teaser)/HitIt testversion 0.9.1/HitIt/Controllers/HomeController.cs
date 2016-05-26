02abd041d35ca633b984f790902a0e1b22b6bd41
MaximilianAlmo <maximilian.almo@gmail.com>
2016-05-26 02:54:30 +0200

    Here we go!
    Teasern fungerar (delvis)

---
+++ HitIt (Nu med profilbild och teaser)/HitIt testversion 0.9.1/HitIt/Controllers/HomeController.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data;
+using System.Data.Entity;
+using System.Net;
+using HitIt.Models;
+using System.IO;
+using MySql.Data.MySqlClient;
+
+namespace HitIt.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Data()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Data(HttpPostedFileClass profile2, HttpPostedFileClass profileTeaser)
+        {
+
+            if (profile2.file.ContentLength > 0)
+            {
+                var fileName = Path.GetFileName(profile2.file.FileName);
+                var path = Path.Combine(Server.MapPath("~/Content/Pictures/profilePictures"), fileName);
+                profile2.file.SaveAs(path);
+
+                string connectString = "SERVER= 195.178.232.16; PORT=3306; DATABASE=af6947;UID=af6947 ;PASSWORD=sexyKalle79;";
+                MySqlConnection connection = new MySqlConnection(connectString);
+                connection.Open();
+                MySqlCommand cmd = connection.CreateCommand();
+                cmd.CommandText = "SELECT id FROM profile WHERE UserName ='" + User.Identity.Name + "'";
+                Int32 userEdit = (Int32)cmd.ExecuteScalar();
+                cmd.CommandText = "UPDATE profile SET picturePath=@picturePath WHERE UserName ='" + User.Identity.Name + "' AND id ='" + userEdit + "'";
+                cmd.Parameters.Add("@UserName", userEdit);
+                cmd.Parameters.Add("@picturePath", fileName);
+                cmd.ExecuteNonQuery();
+                //connection.Close();
+
+                var fileNameTeaser = Path.GetFileName(profileTeaser.fileTeaser.FileName);
+                var pathTeaser = Path.Combine(Server.MapPath("~/Content/MusicUploads/profileTeasers"), fileNameTeaser);
+                profileTeaser.fileTeaser.SaveAs(pathTeaser);
+
+                //string connectString = "SERVER= 195.178.232.16; PORT=3306; DATABASE=af6947;UID=af6947 ;PASSWORD=sexyKalle79;";
+                //MySqlConnection connection = new MySqlConnection(connectString);
+                //connection.Open();
+                //MySqlCommand cmd = connection.CreateCommand();
+                cmd.CommandText = "SELECT id FROM profile WHERE UserName ='" + User.Identity.Name + "'";
+                //Int32 userEdit = (Int32)cmd.ExecuteScalar();
+                cmd.CommandText = "UPDATE profile SET teaserPath=@teaserPath WHERE UserName ='" + User.Identity.Name + "' AND id ='" + userEdit + "'";
+                cmd.Parameters.Add("@teaserPath", fileNameTeaser);
+                cmd.ExecuteNonQuery();
+                connection.Close();
+
+            }
+            //if (profile2.fileTeaser.ContentLength > 0)
+            //{
+            //    var fileNameTeaser = Path.GetFileName(profile2.fileTeaser.FileName);
+            //    var pathTeaser = Path.Combine(Server.MapPath("~/Content/MusicUploads/profileTeasers"), fileNameTeaser);
+            //    profile2.fileTeaser.SaveAs(pathTeaser);
+
+            //    string connectString = "SERVER= 195.178.232.16; PORT=3306; DATABASE=af6947;UID=af6947 ;PASSWORD=sexyKalle79;";
+            //    MySqlConnection connection = new MySqlConnection(connectString);
+            //    connection.Open();
+            //    MySqlCommand cmd = connection.CreateCommand();
+            //    cmd.CommandText = "SELECT id FROM profile WHERE UserName ='" + User.Identity.Name + "'";
+            //    Int32 userEdit = (Int32)cmd.ExecuteScalar();
+            //    cmd.CommandText = "UPDATE profile SET teaserPath=@teaserPath WHERE UserName ='" + User.Identity.Name + "' AND id ='" + userEdit + "'";
+            //    cmd.Parameters.Add("@UserName", userEdit);
+            //    cmd.Parameters.Add("@teaserPath", fileNameTeaser);
+            //    cmd.ExecuteNonQuery();
+            //    connection.Close();
+
+            //}
+
+            return View();
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Profile()
+        {
+            ViewBag.Message = "Your profile page.";
+
+            return View();
+        }
+        public ActionResult Collaborations()
+        {
+            ViewBag.Message = "Your profile page.";
+
+            return View();
+        }
+    }
+}
---
+++ HitIt (Nu med profilbild och teaser)/HitIt testversion 0.9.1/HitIt/Controllers/PictureController.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using HitIt.Models;
+using System.IO;
+using MySql.Data.MySqlClient;
+
+namespace HitIt.Controllers
+{
+    public class PictureController : Controller
+    {
+        //
+        // GET: /Picture/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //
+        // GET: /Picture/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        //
+        // GET: /Picture/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Picture/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //
+        // GET: /Picture/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        //
+        // POST: /Picture/Edit/5
+        [HttpPost]
+        public ActionResult Edit(HttpPostedFileClass profile2)
+        {
+            
+        
+        
+            return RedirectToAction("Index");
+        }
+
+        //
+        // GET: /Picture/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        //
+        // POST: /Picture/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
